8fa51856230b8d278d18e5d641d6db1201ec8579
Lokkanet <113406930+bagheeraja@users.noreply.github.com>
2026-05-05 10:43:48 -0400

    Unit 1 Session 2 Pset 2 Problem 5: missing number revision

--- missing_number.py
+++ missing_number.py
@@ -10,8 +10,27 @@ def find_missing(nums: [list]):
 
     return missing
 
-nums = [2,4,1,0,5]
+def find_missing_iter(nums: [list]):
+    nums.sort()
+
+    for i in range(len(nums)):
+        if i != nums[i]:
+            return i
+        
+def find_missing_enum(nums: [list]):
+    
+    for i, num in enumerate(nums):
+        if i != num:
+            return i
+
+nums = [2,4,1,0,5,3,6,10,9,8]
 missing_num = find_missing(nums)
 print(missing_num)
+
+missing_num2 = find_missing_iter(nums)
+print(missing_num2)
+
+missing_num3 = find_missing_enum(nums)
+print(missing_num3)
 
 print(1^2^1)
