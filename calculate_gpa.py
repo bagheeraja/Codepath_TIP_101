7e173d36a682aa177e816e74640653068eb1aaec
Lokkanet <113406930+bagheeraja@users.noreply.github.com>
2026-05-05 21:49:59 -0400

    Unit 2 Session 1 Pset 1 Problem 6: calculate gpa

---
+++ calculate_gpa.py
@@ -0,0 +1,11 @@
+def calculate_gpa(report_card: dict[str,str]) -> float:
+    grades = {"A": 4, "B": 3, "C": 2, "D": 1, "F": 0}
+    total = 0
+
+    for course, grade in report_card.items():
+        total += grades[grade]
+
+    return total/len(report_card)
+
+report_card = {"Math": "A", "Science": "C", "History": "A", "Art": "B", "English": "B", "Spanish": "A"}
+print(calculate_gpa(report_card))
